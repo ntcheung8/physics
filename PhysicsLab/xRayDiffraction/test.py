9030172e4cb31b4081823f67d94de046fe0343dc
ntcheung8 <ntcheung18@gmail.com>
2019-11-26 00:17:09 -0500

    xRayDiffraction code added

---
+++ PhysicsLab/xRayDiffraction/test.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 25 22:25:45 2019
+
+@author: Nicholas Cheung
+"""
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+data = pd.read_excel("data.xlsx")
+print(data)
